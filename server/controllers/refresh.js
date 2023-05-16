459496054c8e2b641b26716e790e60fe2d8bfc73
Mahir <mahdimahir45@gmail.com>
2023-05-16 10:16:44 +0600

    server v2.0

--- server/controllers/refresh.js
+++ server/controllers/refresh.js
@@ -1,48 +1,49 @@
-const {User} = require('../models/user');
-const jwt = require('jsonwebtoken');
+const { User } = require("../models/user");
+const jwt = require("jsonwebtoken");
 
-const RefreshTokenHandler = (req,res) =>{
+const RefreshTokenHandler = (req, res) => {
+  //refresh token is sent through cookies
+  const cookies = req.cookies;
 
-    //refresh token is sent through cookies
-    const cookies = req.cookies;
+  //checks if refresh token exists
+  if (cookies?.jwt) {
+    const refreshToken = cookies.jwt;
+    jwt.verify(
+      refreshToken,
+      process.env.REFRESH_TOKEN_SECRET,
+      async (err, decoded) => {
+        if (err) {
+          res.status(403).json({ message: "Forbidden" });
+        } else {
+          const user = await User.findOne({ username: decoded.username });
+          if (user) {
+            const { username, _id, img, email, conversation } = user;
+            const accessToken = jwt.sign(
+              {
+                UserInfo: {
+                  username: user.username,
+                  img: user.img,
+                  id: user._id,
+                },
+              },
+              process.env.ACCESS_TOKEN_SECRET,
+              { expiresIn: "900s" }
+            );
 
-    //checks if refresh token exists
-    if(cookies?.jwt){
-        const refreshToken = cookies.jwt;
-        jwt.verify(
-            refreshToken,
-            process.env.REFRESH_TOKEN_SECRET,
-            async(err, decoded)=>{
-                if(err){
-                    res.status(403).json({message:"Forbidden"})
-                }else{
-                    const user = await User.findOne({username: decoded.username})
-                    if(user){
-                        const {username, _id, img, email,conversation} = user
-                        const accessToken = jwt.sign(
-                            {'UserInfo' : {
-                                'username': user.username,
-                                'img': user.img,
-                                'id': user._id
-                            }},
-                            process.env.ACCESS_TOKEN_SECRET,
-                            {expiresIn:'900s'}
-                        )
-                        
-                        //sends new access token 
-                        res.json({user:{username,email,_id,img,conversation},accessToken})
-                    }else{
-                        res.status(401).json({message:'Unauthorized user'})
-                    }
-                }
-            }
-        )
-    }else{
-        res.status(401).json({message: "Unauthorized Token"})
-    }
+            //sends new access token
+            res.json({
+              user: { username, email, _id, img, conversation },
+              accessToken,
+            });
+          } else {
+            res.status(401).json({ message: "Unauthorized user" });
+          }
+        }
+      }
+    );
+  } else {
+    res.status(401).json({ message: "Unauthorized Token" });
+  }
+};
 
-
-    //verifies refresh token
-}
-
-module.exports = RefreshTokenHandler
+module.exports = RefreshTokenHandler;
